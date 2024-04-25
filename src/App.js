387678c2ee543ee00e7ef1f356bf6f9feb1564e4
Idan Maimon <idanmaimon381891@gmail.com>
2024-04-26 00:32:22 +0300

    created a react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './assets/scss/global.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div >
+  )
+}
+
+export default App
