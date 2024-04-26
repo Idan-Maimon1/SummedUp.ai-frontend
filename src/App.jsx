c6aed10f76920c756fd64d0b11fbd484159927a7
Idan Maimon <idanmaimon381891@gmail.com>
2024-04-26 16:34:29 +0300

    appHeader init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AppHeader from './components/AppHeader'
+import './assets/scss/global.scss'
+
+export default function App() {
+  return (
+    <>
+      <AppHeader />
+    </>
+  )
+}
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import summeduplogo from '../assets/imgs/SummedUp-logo.png'
+
+export default function AppHeader() {
+    return (
+        <section className="app-header">
+            <img src={summeduplogo} alt="" />
+        </section>
+    )
+}
