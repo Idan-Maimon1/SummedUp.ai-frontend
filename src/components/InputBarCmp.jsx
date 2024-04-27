b0943c40347a698ace76a5c85731f87c3cf637a7
Idan Maimon <idanmaimon381891@gmail.com>
2024-04-27 22:49:36 +0300

    url validation and videoId extraction

--- src/components/InputBarCmp.jsx
+++ src/components/InputBarCmp.jsx
@@ -2,11 +2,39 @@ import React, { Component } from 'react'
 import SearchIcon from '../assets/imgs/search-icon.svg'
 
 export default class InputBarCmp extends Component {
+    state = {
+        inputUrl: '',
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ inputUrl: event.target.value })
+    }
+
+    extractYouTubeVideoId = () => {
+        const { inputUrl } = this.state
+        const youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})/
+        const match = inputUrl.match(youtubeRegex)
+        if (match) {
+            const videoId = match[1]
+            this.props.handleVideoIdChange(videoId)
+        } else {
+            this.setState({ videoId: null, inputUrl: '' })
+            alert('Invalid YouTube URL.')
+        }
+    }
+
     render() {
+        const { inputUrl } = this.state
+
         return (
             <section className="input-bar-cont">
-                <input type="text" placeholder='Youtube video url' />
-                <button className='search-icon-cont'>
+                <input
+                    type="text"
+                    placeholder="Youtube video url"
+                    value={inputUrl}
+                    onChange={this.handleInputChange}
+                />
+                <button className="search-icon-cont" onClick={this.extractYouTubeVideoId}>
                     <img src={SearchIcon} alt="" />
                 </button>
             </section>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
-import InputBarCmp from '../components/InputBarCmp'
 import HeroCmp from '../components/HeroCmp'
+import InputBarCmp from '../components/InputBarCmp'
+import ContentManager from '../components/ContentManager'
 
 export default class LandingPage extends Component {
+    state = {
+        videoId: null,
+    }
+
+    handleVideoIdChange = (videoId) => {
+        this.setState({ videoId })
+    }
+
     render() {
+        const { videoId } = this.state
         return (
             <>
                 <section className='lnding-page-cont'>
                     <div className="landing-page-layout">
                         <HeroCmp />
-                        <InputBarCmp />
+                        <InputBarCmp handleVideoIdChange={this.handleVideoIdChange} />
+                        {videoId && <ContentManager videoId={videoId} />}
                     </div>
                 </section>
             </>
